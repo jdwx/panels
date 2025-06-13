be8fcad89aed76860c5983127879b5eb97df9eb7
JDW <xs@jdw.sx>
2025-06-12 20:05:29 -0400

    Make stricter.

--- tests/ElementPanelTest.php
+++ tests/ElementPanelTest.php
@@ -5,6 +5,8 @@ declare( strict_types = 1 );
 
 
 use JDWX\Panels\ElementPanel;
+use JDWX\Strict\Cast;
+use JDWX\Strict\TypeIs;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
@@ -14,65 +16,61 @@ final class ElementPanelTest extends TestCase {
 
 
     public function testBodyForIterable() : void {
-        $el = new class() extends ElementPanel {
-
-
-            /** @return iterable<string> */
-            protected function innerBody() : iterable {
-                yield 'foo';
-            }
-
-
-        };
-        self::assertSame( [ '<div>', 'foo', '</div>' ], iterator_to_array( $el->body(), false ) );
+        $el = $this->newElementPanel( [ 'foo', 'bar', 'baz' ] );
+        self::assertSame(
+            [ '<div>', 'foo', 'bar', 'baz', '</div>' ],
+            Cast::listStringy( TypeIs::iterable( $el->body() ) )
+        );
     }
 
 
     public function testBodyForString() : void {
-        $el = new class() extends ElementPanel {
-
-
-            protected function innerBody() : string {
-                return 'bar';
-            }
-
-
-        };
-        self::assertSame( [ '<div>', 'bar', '</div>' ], iterator_to_array( $el->body(), false ) );
+        $el = $this->newElementPanel( 'bar' );
+        self::assertSame( [ '<div>', 'bar', '</div>' ], Cast::listStringy( TypeIs::iterable( $el->body() ) ) );
     }
 
 
     public function testSetAttribute() : void {
-        $el = new class() extends ElementPanel {
-
-
-            protected function innerBody() : string {
-                return 'baz';
-            }
-
-
-        };
+        $el = $this->newElementPanel( 'baz' );
         $el->setAttribute( 'id', 'bar' );
         $el->setAttribute( 'contenteditable' );
         self::assertSame(
             [ '<div contenteditable id="bar">', 'baz', '</div>' ],
-            iterator_to_array( $el->body(), false )
+            Cast::listStringy( TypeIs::iterable( $el->body() ) )
         );
     }
 
 
     public function testSetElement() : void {
-        $el = new class() extends ElementPanel {
+        $el = $this->newElementPanel( 'baz' );
+        $el->setElement( 'span' );
+        self::assertSame( [ '<span>', 'baz', '</span>' ], Cast::listStringy( TypeIs::iterable( $el->body() ) ) );
+    }
 
 
-            protected function innerBody() : string {
-                return 'baz';
+    /** @param list<string>|string $stBody */
+    private function newElementPanel( array|string $stBody ) : ElementPanel {
+        return new class( $stBody ) extends ElementPanel {
+
+
+            /** @var list<string>|string */
+            private array|string $stBody;
+
+
+            /** @param list<string>|string $i_stBody */
+            public function __construct( array|string $i_stBody ) {
+                parent::__construct();
+                $this->stBody = $i_stBody;
+            }
+
+
+            /** @return list<string>|string */
+            protected function innerBody() : array|string {
+                return $this->stBody;
             }
 
 
         };
-        $el->setElement( 'span' );
-        self::assertSame( [ '<span>', 'baz', '</span>' ], iterator_to_array( $el->body(), false ) );
     }
 
 
--- tests/SimpleElementPanelTest.php
+++ tests/SimpleElementPanelTest.php
@@ -5,6 +5,8 @@ declare( strict_types = 1 );
 
 
 use JDWX\Panels\SimpleElementPanel;
+use JDWX\Strict\Cast;
+use JDWX\Strict\TypeIs;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
@@ -19,14 +21,14 @@ final class SimpleElementPanelTest extends TestCase {
         $panel->addBody( 'qux' );
         self::assertSame(
             [ '<foo>', 'bar', 'baz', 'qux', '</foo>' ],
-            iterator_to_array( $panel->body(), false )
+            Cast::listStringy( TypeIs::iterable( $panel->body() ) )
         );
     }
 
 
     public function testConstruct() : void {
         $panel = new SimpleElementPanel( 'foo', 'bar' );
-        self::assertSame( [ '<foo>', 'bar', '</foo>' ], iterator_to_array( $panel->body(), false ) );
+        self::assertSame( [ '<foo>', 'bar', '</foo>' ], Cast::listStringy( TypeIs::iterable( $panel->body() ) ) );
     }
 
 
